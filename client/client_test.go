920212d166878a540d0af1d5256677573403cd10
wccai <wccai@easyops>
2026-06-09 15:55:34 +0800

    test: cover processLoop shutdown paths

--- client/client_test.go
+++ client/client_test.go
@@ -24,7 +24,9 @@ func TestDingtalkOpenStreamClient_Start(t *testing.T) {
 func TestDingtalkOpenStreamClient_processDataFrame(t *testing.T) {
 }
 
-func TestDingtalkOpenStreamClient_processLoop_PongHandlerAfterExitDoesNotPanic(t *testing.T) {
+func newTestWebsocketConn(t *testing.T, serverHandler func(*websocket.Conn)) *websocket.Conn {
+	t.Helper()
+
 	upgrader := websocket.Upgrader{}
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		conn, err := upgrader.Upgrade(w, r, nil)
@@ -32,20 +34,30 @@ func TestDingtalkOpenStreamClient_processLoop_PongHandlerAfterExitDoesNotPanic(t
 			t.Errorf("upgrade websocket: %v", err)
 			return
 		}
-		_ = conn.Close()
+		go serverHandler(conn)
 	}))
-	defer server.Close()
 
 	wsURL := "ws" + strings.TrimPrefix(server.URL, "http")
 	conn, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
 	if err != nil {
+		server.Close()
 		t.Fatalf("dial websocket: %v", err)
 	}
+	t.Cleanup(func() {
+		_ = conn.Close()
+		server.Close()
+	})
+
+	return conn
+}
+
+func runProcessLoop(t *testing.T, conn *websocket.Conn, keepAliveIdle time.Duration) <-chan struct{} {
+	t.Helper()
 
 	cli := &StreamClient{
 		conn:          conn,
 		AutoReconnect: false,
-		keepAliveIdle: time.Hour,
+		keepAliveIdle: keepAliveIdle,
 	}
 
 	done := make(chan struct{})
@@ -53,12 +65,55 @@ func TestDingtalkOpenStreamClient_processLoop_PongHandlerAfterExitDoesNotPanic(t
 		defer close(done)
 		cli.processLoop()
 	}()
+	return done
+}
+
+func waitProcessLoopExit(t *testing.T, done <-chan struct{}, timeout time.Duration) {
+	t.Helper()
 
 	select {
 	case <-done:
-	case <-time.After(time.Second):
+	case <-time.After(timeout):
 		t.Fatal("processLoop did not exit")
 	}
+}
+
+func TestDingtalkOpenStreamClient_processLoop_ReadErrorCancelsLoop(t *testing.T) {
+	conn := newTestWebsocketConn(t, func(conn *websocket.Conn) {
+		_ = conn.Close()
+	})
+
+	done := runProcessLoop(t, conn, time.Hour)
+
+	waitProcessLoopExit(t, done, time.Second)
+}
+
+func TestDingtalkOpenStreamClient_processLoop_PingTimeoutCancelsLoop(t *testing.T) {
+	conn := newTestWebsocketConn(t, func(conn *websocket.Conn) {
+		conn.SetPingHandler(func(string) error {
+			return nil
+		})
+		for {
+			if _, _, err := conn.NextReader(); err != nil {
+				_ = conn.Close()
+				return
+			}
+		}
+	})
+
+	done := runProcessLoop(t, conn, 10*time.Millisecond)
+
+	waitProcessLoopExit(t, done, 6*time.Second)
+}
+
+func TestDingtalkOpenStreamClient_processLoop_PongHandlerAfterExitDoesNotPanic(t *testing.T) {
+	conn := newTestWebsocketConn(t, func(conn *websocket.Conn) {
+		_ = conn.Close()
+	})
+
+	done := runProcessLoop(t, conn, time.Hour)
+
+	waitProcessLoopExit(t, done, time.Second)
 
 	pongHandler := conn.PongHandler()
 	for i := 0; i < 100; i++ {
